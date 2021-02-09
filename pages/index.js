bb5d454eef6541e8f22d4006ad638f02f857c451
charles <codelyfewithcharlie@gmail.com>
2021-02-09 06:45:28 -0500

    updates

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import PriceCard from '../components/priceCard';
 import styles from '../styles/index.module.scss';
+import Image from 'next/image';
+
 export default function Home() {
   return (
     <div>
@@ -9,8 +11,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.png" />
       </Head>
       <section className={styles.heading_container}>
+        <div className={styles.heading_img}>
+          <Image
+            src="/pattern-circles.svg"
+            alt="Picture of the author"
+            width={120}
+            height={120}
+          />
+        </div>
+
         <h1>Simple, traffic-based pricing</h1>
-        <h2>Sign-up for our 30-day trial</h2>
+        <h2>Sign-up for our 30-day trial.</h2>
         <h2>No credit card required.</h2>
       </section>
       <PriceCard />
