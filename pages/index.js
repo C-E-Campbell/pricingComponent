c15b28034c82820511871d4645be9730006e935b
charles <codelyfewithcharlie@gmail.com>
2021-02-09 06:30:09 -0500

    updates

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import PriceCard from '../components/priceCard';
+import styles from '../styles/index.module.scss';
 export default function Home() {
   return (
     <div>
@@ -7,7 +8,7 @@ export default function Home() {
         <title>React Pricing Component</title>
         <link rel="icon" href="/favicon.png" />
       </Head>
-      <section>
+      <section className={styles.heading_container}>
         <h1>Simple, traffic-based pricing</h1>
         <h2>Sign-up for our 30-day trial</h2>
         <h2>No credit card required.</h2>
