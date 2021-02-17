c98eaca48d6d62902bc25c19a9e33fbc04eb30b9
charles <codelyfewithcharlie@gmail.com>
2021-02-17 08:05:42 -0500

    updates

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
         <title>React Pricing Component</title>
         <link rel="icon" href="/favicon.png" />
       </Head>
-      <div>
+      <div className={styles.topContainer}>
         <img
           className={styles.svg}
           src="/bg-pattern.svg"
